6a69fd8710279e3608fe5c81580190cb7463242b
世界 <sekai@neko.services>
2021-07-14 20:58:57 +0800

    Pkg name in tun2socks

--- tun2socks/tun2socks.c
+++ tun2socks/tun2socks.c
@@ -411,7 +411,7 @@ int wait_for_fd()
     }
     fcntl(sock, F_SETFL, flags | O_NONBLOCK);
 
-    char *path = "/data/data/com.github.shadowsocks/sock_path";
+    char *path = "/data/data/io.nekohasekai.sagernet/sock_path";
     if (options.sock_path != NULL) {
         path = options.sock_path;
     }
@@ -487,8 +487,8 @@ int main (int argc, char **argv)
 
     if (options.fake_proc) {
         // Fake process name to cheat on Lollipop
-        strcpy(argv[0], "com.github.shadowsocks");
-        prctl(PR_SET_NAME, "com.github.shadowsocks");
+        strcpy(argv[0], "io.nekohasekai.sagernet");
+        prctl(PR_SET_NAME, "io.nekohasekai.sagernet");
     }
 
     // handle --help and --version
